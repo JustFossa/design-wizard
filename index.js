cec073f4bfb2c0a570da5f8f2c3b05ba346745e0
Fossa <marecocico4@gmail.com>
2021-07-03 22:46:38 +0200

    ok

---
+++ index.js
@@ -0,0 +1,8 @@
+function designwizard(options) {
+    let images = document.querySelectorAll(".design-wizard")
+
+    if (options.shadow_type === "hard")
+        options.shadow_type === "0px"
+    else 
+        options.shadow_type === "15px"
+}
